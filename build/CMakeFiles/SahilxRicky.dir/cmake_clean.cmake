9eafd02d12ded67016d4a73c8f24708d48341cd2
rjrios915 <rjrios@stanford.edu>
2024-08-07 22:18:54 -0700

    HELLO WORLD!

---
+++ build/CMakeFiles/SahilxRicky.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SahilxRicky.dir/src/Main.cpp.o"
+  "CMakeFiles/SahilxRicky.dir/src/Main.cpp.o.d"
+  "SahilxRicky"
+  "SahilxRicky.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SahilxRicky.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/SahilxRicky.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SahilxRicky.
+# This may be replaced when dependencies are built.
