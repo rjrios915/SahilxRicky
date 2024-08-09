460f6fcaf5f10d0284469ee68775f10d77465077
rjrios915 <rjrios@stanford.edu>
2024-08-08 19:19:35 -0700

    Compiled in C

--- build/CMakeFiles/main.dir/DependInfo.cmake
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/ricky/Desktop/SahilxRicky/src/Main.cpp" "CMakeFiles/main.dir/src/Main.cpp.o" "gcc" "CMakeFiles/main.dir/src/Main.cpp.o.d"
+  "/Users/ricky/Desktop/SahilxRicky/src/main.c" "CMakeFiles/main.dir/src/main.c.o" "gcc" "CMakeFiles/main.dir/src/main.c.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- build/CMakeFiles/main.dir/cmake_clean.cmake
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -1,11 +1,11 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/main.dir/src/Main.cpp.o"
-  "CMakeFiles/main.dir/src/Main.cpp.o.d"
+  "CMakeFiles/main.dir/src/main.c.o"
+  "CMakeFiles/main.dir/src/main.c.o.d"
   "main"
   "main.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C)
   include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
